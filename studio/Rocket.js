52610306dc2b4629c32c03ec5ef2eb1bd79c9e19
Joe Bao <toj7278@yahoo.com>
2022-03-15 23:17:35 -0500

    started on studio, need to finish last two parts

---
+++ studio/Rocket.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+exports.Rocket = void 0;
+var Rocket = /** @class */ (function () {
+    function Rocket(name, totalCapacityKg) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    Rocket.prototype.sumMass = function (items) {
+        var sum = 0;
+        for (var i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        }
+        return sum;
+    };
+    return Rocket;
+}());
+exports.Rocket = Rocket;
